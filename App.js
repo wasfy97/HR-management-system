bb17f6b02373e274b45dc4d1fe5f4efbd6ceac16
wasfy97 <hadiwasfy@gmail.com>
2023-05-29 20:16:24 +0000

    calculate the net salary not salary

--- App.js
+++ App.js
@@ -1,82 +1,64 @@
-// Define the employee objects
-const employees = [
-    {
-      employeeID: 1000,
-      fullName: "Ghazi Samer",
-      department: "Administration",
-      level: "Senior",
-      imageURL: "https://example.com/ghazi-samer.jpg"
-    },
-    {
-      employeeID: 1001,
-      fullName: "Lana Ali",
-      department: "Finance",
-      level: "Senior",
-      imageURL: "https://example.com/lana-ali.jpg"
-    },
-    {
-      employeeID: 1002,
-      fullName: "Tamara Ayoub",
-      department: "Marketing",
-      level: "Senior",
-      imageURL: "https://example.com/tamara-ayoub.jpg"
-    },
-    {
-      employeeID: 1003,
-      fullName: "Safi Walid",
-      department: "Administration",
-      level: "Mid-Senior",
-      imageURL: "https://example.com/safi-walid.jpg"
-    },
-    {
-      employeeID: 1004,
-      fullName: "Omar Zaid",
-      department: "Development",
-      level: "Senior",
-      imageURL: "https://example.com/omar-zaid.jpg"
-    },
-    {
-      employeeID: 1005,
-      fullName: "Rana Saleh",
-      department: "Development",
-      level: "Junior",
-      imageURL: "https://example.com/rana-saleh.jpg"
-    },
-    {
-      employeeID: 1006,
-      fullName: "Hadi Ahmad",
-      department: "Finance",
-      level: "Mid-Senior",
-      imageURL: "https://example.com/hadi-ahmad.jpg"
+'use strict'
+function Employee(fullName, department, level) {
+  this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+
+  }
+
+
+  Employee.prototype.generateEmployeeId = function() {
+    this.id = Math.floor(Math.random() * 9000) + 1000;
+  };
+
+
+  Employee.prototype.calculateSalary = function() {
+    let minSalary, maxSalary;
+    switch (this.level) {
+      case "Senior":
+        minSalary = 1500;
+        maxSalary = 2000;
+        break;
+      case "Mid-Senior":
+        minSalary = 1000;
+        maxSalary = 1500;
+        break;
+      case "Junior":
+        minSalary = 500;
+        maxSalary = 1000;
+        break;
+      default:
+        minSalary = 0;
+        maxSalary = 0;
     }
+    const randomSalary = Math.floor(Math.random() * (maxSalary - minSalary + 1)) + minSalary;
+    this.salary = randomSalary - (randomSalary * 7.5) / 100;
+  };
+  
+  Employee.prototype.render = function() {
+    document.write('Employee name: ' + this.fullName + '<br>');
+    document.write('Department: ' + this.department + '<br>');
+    document.write('Employee salary: ' + this.salary + '<br><br>');
+  };
+
+
+  let employees = [
+    new Employee('Ghazi Samer', 'Administration', 'Senior'),
+    new Employee('Lana Ali', 'Finance', 'Senior'),
+    new Employee('Tamara Ayoub', 'Marketing', 'Senior'),
+    new Employee('Safi Walid', 'Administration', 'Mid-Senior'),
+    new Employee('Omar Zaid', 'Development', 'Senior'),
+    new Employee('Rana Saleh', 'Development', 'Junior'),
+    new Employee('Hadi Ahmad', 'Finance', 'Mid-Senior')
   ];
-  
-  // Calculate salary for each employee
-  function calculateSalary(employee) {
-    const levelSalary = {
-      Senior: { min: 1500, max: 2000 },
-      "Mid-Senior": { min: 1000, max: 1500 },
-      Junior: { min: 500, max: 1000 }
-    };
-  
-    const { min, max } = levelSalary[employee.level];
-    const randomSalary = Math.floor(Math.random() * (max - min + 1)) + min;
-    const netSalary = randomSalary - (randomSalary * 0.075); // 7.5% tax
-  
-    employee.salary = netSalary.toFixed(2);
+
+
+  for (var i = 0; i < employees.length; i++) {
+    employees[i].generateEmployeeId();
+    employees[i].calculateSalary();
   }
-  
- // Render employees' names with their salaries
-function renderEmployees() {
-    employees.forEach(employee => {
-      calculateSalary(employee);
-  
-      const employeeInfo = `Employee name: ${employee.fullName}<br>Employee salary: ${employee.salary}<br><br>`;
-      document.write(employeeInfo);
-    });
+
+
+  for (var i = 0; i < employees.length; i++) {
+    employees[i].render();
   }
-  
-  // Call the render function
-  renderEmployees();
-  
-  
